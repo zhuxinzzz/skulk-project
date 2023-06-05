2cd6cdee4aee5c1ed62f45ef55cc78eb726a425f
zzz <no e-mail>
2023-06-05 19:28:23 +0800

    add demo mq

---
+++ demo-spring-boot-kafka/consumer/src/main/java/com/MyConsumer.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyConsumer {
+
+    @KafkaListener(topics = "mytopic")
+    public void listen(ConsumerRecord<?, String> record) {
+        String value = record.value();
+        System.out.println(value);
+        System.out.println(record);
+    }
+}
